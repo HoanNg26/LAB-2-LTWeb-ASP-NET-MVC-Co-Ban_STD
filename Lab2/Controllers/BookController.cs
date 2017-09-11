c3c953ad477e3d62a796499ed60c916e3034b176
Administrator <Administrator@PHANTRUNGHIEU>
2017-09-11 15:01:14 +0700

    Add project files.

---
+++ Lab2/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using Lab2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public string HelloTeacher(string university)
+        {
+            return "Nguyen Dinh Anh - University: " + university;
+        }
+        public ActionResult ListBook()
+        {
+            var books = new List<string>();
+            books.Add("HTML5 & CSS The complete manual - Author Name Book");
+            books.Add("HTML5 & CSS Responsive web Design cookbook - Author Name Book 2");
+            books.Add("Professional ASP.NET - Author Name Book 2");
+            ViewBag.Books = books;
+            return View();
+        }
+
+        public ActionResult ListBookModel()
+        {
+            var books = new List<Book>();
+            books.Add(new Book(1, "HTML5 & CSS The complete manual", "Author Name Book 1", "/Content/Images/book1cover.jpg"));
+            books.Add(new Book(2, "HTML5 & CSS Responsive web Design cookbook", "Author Name Book 2", "/Content/Images/book2cover.jpg"));
+            books.Add(new Book(3, "Professional ASP.NET", "Author Name Book 3", "/Content/Images/book3cover.jpg"));
+            return View(books);
+        }
+    }
+}
